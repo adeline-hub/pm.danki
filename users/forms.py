86a4b79975c898ab54557c83b69ea1fa360565ad
adeline <59871821+adeline-hub@users.noreply.github.com>
2024-09-05 15:32:28 +0200

    Delete users directory

--- users/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Profile
-from django.forms import ModelForm
-
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model =Profile
-        fields = ['name','email','bio','photo']
